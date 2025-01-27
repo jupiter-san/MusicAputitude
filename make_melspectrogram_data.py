b48a8f316b61dbc61f1abe05c9a3ebbc8deea7ae
jupiter-san <jupiter_san@triton.ocn.ne.jp>
2025-01-27 20:53:21 +0900

    1st

---
+++ make_melspectrogram_data.py
@@ -0,0 +1,64 @@
+import os
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 曲をメルスペクトログラムに変換し、画像にして保存するSUB
+def melspectrogram_conv(y,sr,s_name):
+    # n_mels is number of Mel bands to generate
+    n_mels=128
+    # hop_length is number of samples between successive frames.
+    hop_length=2068
+    # n_fft is length of the FFT window
+    n_fft=2048
+    # Passing through arguments to the Mel filters
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, hop_length=hop_length, n_fft=n_fft)
+    log_S = librosa.power_to_db(S, ref=np.max)
+    #print(log_S.shape)
+    plt.figure(figsize=(12, 4))
+    librosa.display.specshow(data=log_S, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Mel spectrogram')
+    plt.tight_layout()
+
+    plt.savefig(s_name,format="png")
+
+
+dir_name ="MusicAputitude/music"
+for current_dir,dirs,files in os.walk(dir_name):
+    for file in files:
+        file_ext = file.rsplit(".")[-1]
+        # mp3データのみ以下の処理を行う
+        if file_ext == "mp3":
+            file_name = os.path.join(current_dir,file)
+            file_noext = os.path.splitext(file)[0]
+            new_file_path = os.path.join(current_dir.rsplit("\\")[-1].replace("mp3_data", "image_data")) 
+
+            # 保存ディレクトリが存在しないと書き出し時にエラーになるので、存在確認＆作成
+            if not os.path.exists(new_file_path):
+                os.makedirs(new_file_path)
+        
+            #print(new_file_path)
+            offset = 0.0
+            i = 0
+            # Load a flac file from 0(s) to 60(s) and resample to 4.41 KHz
+            y, sr = librosa.load(file_name, sr=4410, offset=offset, duration=60.0)
+            len_y = y.shape[0] / sr
+            # 曲のはじめから60秒ごとに分割。60秒未満は捨てる。
+            while len_y == 60:
+                # メルスペクトグラム画像の書き出し
+                s_name = os.path.join(new_file_path,f'{file_noext}_{i:02}.png')
+                melspectrogram_conv(y,sr,s_name)
+                # 次の分割の読み込み
+                offset += 60
+                y, sr = librosa.load(file_name, sr=4410, offset=offset, duration=60.0)
+                len_y = y.shape[0] / sr
+                i += 1
+                
+            # offset 開始秒を60秒ずつずらすことで、60秒ごとにデータを分割
+            # 画像出力にはplt.savefigを使う
+            #　music/{musician}/image_data/ファイル名_01.png
+
+            # pandasでcsv　ファイルパス、教師ラベル
+
---
+++ music_visualization.py
@@ -0,0 +1,35 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+# Load a flac file from 0(s) to 60(s) and resample to 4.41 KHz
+filename = 'MusicAputitude/music/ARASHI/mp3_data/ARASHI - ｢未完｣ [Official Music Video].mp3'
+y, sr = librosa.load(filename, sr=4410, offset=0.0, duration=60.0)
+#librosa.display.waveshow(y=y, sr=sr)
+# sr 1秒間に4410個データが出力される
+# 107031 = 4010 * 24.27     3
+print(y.shape[0] / sr)
+# plt.show()
+#
+# ここからは メル周波数スペクトログラムを可視化
+# n_mels is number of Mel bands to generate
+n_mels=128
+# hop_length is number of samples between successive frames.
+hop_length=2068
+# n_fft is length of the FFT window
+n_fft=2048
+# Passing through arguments to the Mel filters
+# Passing through arguments to the Mel filters
+S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, hop_length=hop_length, n_fft=n_fft)
+
+log_S = librosa.power_to_db(S, ref=np.max)
+print(log_S.shape)
+
+
+plt.figure(figsize=(12, 4))
+librosa.display.specshow(data=log_S, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Mel spectrogram')
+plt.tight_layout()
+
+plt.show()
